84959c6836938e333cefe23e1b541a030929a908
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-28 10:01:14 +0000

    jwtの発行と検証の練習

---
+++ src/main/java/com/example/jwttutorial/jwt/HmacJWTConsumer.java
@@ -0,0 +1,51 @@
+package com.example.jwttutorial.jwt;
+
+import java.util.stream.Collectors;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class HmacJWTConsumer {
+	private String secretKey;
+
+	public HmacJWTConsumer(String secretKey) {
+		this.secretKey = secretKey;
+	}
+
+	public DecodedJWT verifyToken(String token) {
+		//
+		Algorithm alg = Algorithm.HMAC256(secretKey);
+
+		JWTVerifier verifier = JWT.require(alg).withIssuer(HmacJWTProvider.class.getSimpleName())
+				.acceptExpiresAt(5).build();
+
+		try {
+			return verifier.verify(token);
+		} catch (JWTVerificationException e) {
+			System.out.println("jwtトークンの検証に失敗しました");
+			throw e;
+		}
+	}
+
+	public static void main(String[] args) {
+		//
+		String secretKey = System.getenv("SECRET_KEY");
+		DecodedJWT jwt = new HmacJWTConsumer(secretKey)
+				.verifyToken(new HmacJWTProvider(secretKey).generateToken());
+
+		System.out.println("------Decoded JWT ---------");
+		System.out.println("alg: " + jwt.getAlgorithm());
+		System.out.println("content-type: " + jwt.getContentType());
+		System.out.println("typ: " + jwt.getType());
+		System.out.println("issuer: " + jwt.getIssuer());
+		System.out.println("subject: " + jwt.getSubject());
+		System.out.println("expiresAt:" + jwt.getExpiresAt());
+		System.out.println("issuerAt:" + jwt.getIssuedAt());
+		System.out.println("JWT-ID:" + jwt.getId());
+		System.out.println("email:" + jwt.getClaim("email").asString());
+		System.out.println("groups:" + jwt.getClaim("groups").asList(String.class).stream()
+				.collect(Collectors.joining(",")));
+	}
+}
---
+++ src/main/java/com/example/jwttutorial/jwt/HmacJWTProvider.java
@@ -0,0 +1,35 @@
+package com.example.jwttutorial.jwt;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator.Builder;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class HmacJWTProvider {
+	private String secretkey;
+
+	public HmacJWTProvider(String secretkey) {
+		this.secretkey = secretkey;
+	}
+
+	public String generateToken() {
+		//
+		Algorithm alg = Algorithm.HMAC256(this.secretkey);
+
+		String token = JWT.create().withIssuer(this.getClass().getSimpleName())
+				.withSubject("ID12345")
+				.withExpiresAt(OffsetDateTime.now().plusMinutes(60).toInstant())
+				.withIssuedAt(OffsetDateTime.now().toInstant())
+				.withJWTId(UUID.randomUUID().toString()).withClaim("email", "id123459@example.com")
+				.withArrayClaim("groups", new String[] {"member", "admin"}).sign(alg);
+
+		return token;
+	}
+	// public static void main(String[] args) {
+	// 	//
+	// 	String secretKey = System.getenv("SECRET_KEY");
+	// 	System.out.println();
+	// 	System.out.println(new HmacJWTProvider(secretKey).generateToken());
+	// }
+}
