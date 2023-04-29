1e962c3bdf92126784eee1838521e1bfb6f02368
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-29 05:55:45 +0000

    サーバ内部エラーのエラーメッセージをユーザから見えないように修正

--- src/main/java/com/example/jwttutorial/jwtAuthentication/httpRequestRecieveTutorial/error/ApiControllerAdvice.java
+++ src/main/java/com/example/jwttutorial/jwtAuthentication/httpRequestRecieveTutorial/error/ApiControllerAdvice.java
@@ -1,8 +1,10 @@
 package com.example.jwttutorial.jwtAuthentication.httpRequestRecieveTutorial.error;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -22,4 +24,14 @@ public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
 		ResponseError re = new ResponseError(statusCode.value(), ex.getMessage());
 		return super.handleExceptionInternal(ex, re, headers, statusCode, request);
 	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
+		//
+		log.error(ex.getMessage(), ex);
+
+		ResponseError responseError = new ResponseError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "サーバ内で予期せぬエラーが発生しました。管理者に問い合わせてください。");
+		return new ResponseEntity<Object>(responseError, null, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
 }
