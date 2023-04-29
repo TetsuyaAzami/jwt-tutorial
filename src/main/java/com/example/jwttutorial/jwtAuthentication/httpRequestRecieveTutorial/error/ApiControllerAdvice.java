2978b5615ec0d239b435c140ea840c29ed54cb97
Tetsuya Azami <zqianjian35@gmail.com>
2023-04-29 03:23:40 +0000

    ログ出力処理を追加

--- src/main/java/com/example/jwttutorial/jwtAuthentication/httpRequestRecieveTutorial/error/ApiControllerAdvice.java
+++ src/main/java/com/example/jwttutorial/jwtAuthentication/httpRequestRecieveTutorial/error/ApiControllerAdvice.java
@@ -6,14 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestControllerAdvice
+@Slf4j
 public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
 
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
 			HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
 		//
+		log.error(ex.getMessage(), ex);
+
 		ResponseError re = new ResponseError(statusCode.value(), ex.getMessage());
 		return super.handleExceptionInternal(ex, re, headers, statusCode, request);
 	}
